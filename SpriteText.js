27407805765315f20734ca79aca526c74a13841f
Rubén Reyes <ruben.reyes@ficohsa.com>
2018-01-17 01:17:54 -0600

    First functional version.
    Finalized position, rotation, background initialization.

--- SpriteText.js
+++ SpriteText.js
@@ -25,7 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-window.SpriteTextElement = function (params) {
+window.SpriteTextCharacter = function (params, ratio) {
     let __el = {
         __image: null,
         __sx: 0,
@@ -36,6 +36,7 @@ window.SpriteTextElement = function (params) {
         __dy: 0,
         __dWidth: 0,
         __dHeight: 0,
+        __type: 'char',
         init: function(params) {
             if(
                 !params.hasOwnProperty('image') ||
@@ -50,15 +51,56 @@ window.SpriteTextElement = function (params) {
                 this.__sy = params.sy;
                 this.__sWidth = params.sWidth;
                 this.__sHeight = params.sHeight;
+                this.__dWidth = this.__sWidth * ratio;
+                this.__dHeight = this.__sHeight * ratio;
             }
+            return this;
+        },
+        getWidth: function() {
+            return this.__dWidth;
+        },
+        getSourceWidth: function() {
+            return this.__sWidth;
+        },
+        drawImage: function(ctx, p) {
+
+        },
+        getType: function(){
+            return this.__type;
+        },
+        getProperties() {
+            return {
+                image: this.__image,
+                sx: this.__sx,
+                sy: this.__sy,
+                sWidth: this.__sWidth,
+                sHeight: this.__sHeight,
+                dx: this.__dx,
+                dy: this.__dy,
+                dWidth: this.__dWidth,
+                dHeight: this.__dHeight,
+            }
+        },
+    };
+
+    return __el.init(params);
+}
+
+window.SpriteTextSpace = function() {
+    let __el = {
+        __type: 'space',
+        init: function(parms) {
             return this;
         },
         drawImage: function(ctx, p) {
 
+        },
+        getType: function() {
+            return this.__type;
         }
     };
 
-    return __el.init(params);
+    return __el.init();
 }
 
 //////////////////////////////////////////////////////////////
@@ -69,21 +111,29 @@ window.SpriteText = function (canvas,initProps) {
     let __spriteText = {
         __text: "",
         __spritesheets: [],
+        __backgroundImage : null,
         __dimensions: [],
         __canvas: null,
+        __settings: {},
         __properties: {},
+        __iterate: false,
         __defaults__: {
             whitespaceWidth: 0.5, //em units
             caseSensitive: false,
             verticalAlignment: 'middle',
             horizontalAlignment: 'center',
             initialPosition: [0.5,0.5],
+            fontSize: 80, //pixels
+            rotation: 0, //deg
+            rotateBoth: true,
         },
         __elements__: [],
+        __that: null,
         get: function () {
             return this;
         },
         init: function (canvas,initProps) {
+            __that = this;
             this.setCanvas(canvas);
             if (typeof initProps !== 'undefined') {
                 if (initProps.hasOwnProperty('text')) {
@@ -95,9 +145,12 @@ window.SpriteText = function (canvas,initProps) {
                 if (initProps.hasOwnProperty('dimensions')) {
                     this.setDimensions(initProps.dimensions);
                 }
+                if(initProps.hasOwnProperty('backgroundImage')) {
+                    this.setBackgroundImage(initProps.backgroundImage);
+                }
             }
-            this.initProperties(initProps.hasOwnProperty('properties') ? initProps.properties : null);
-            this.createTextElements();
+            this.initSettings(initProps.hasOwnProperty('properties') ? initProps.properties : null);
+            this.checkLoadedImages();
             return this;
         },
 
@@ -106,7 +159,7 @@ window.SpriteText = function (canvas,initProps) {
         },
         setText: function (text) {
             if (typeof text === "string"){
-                if(!this.getProperty('caseSensitive')) {
+                if(!this.getSetting('caseSensitive')) {
                     text = text.toLowerCase();
                 }
                 this.__text = text;
@@ -124,7 +177,11 @@ window.SpriteText = function (canvas,initProps) {
                 for (let valid = true, i = 0; i < images.length; i++) {
                     if (typeof images[i] === "string") {
                         let url = images[i];
-                        images[i] = new Image;
+                        images[i] = new Image();
+                        images[i].loaded = false;
+                        images[i].onload = function() {
+                            this.loaded = true;
+                        }
                         images[i].src = url;
                     }
                     else if (typeof images[i] !== "Image") {
@@ -137,14 +194,49 @@ window.SpriteText = function (canvas,initProps) {
             }
         },
 
+        getBackgroundImage: function() {
+            return this.__backgroundImage;
+        },
+
+        setBackgroundImage: function(image) {
+            if (image instanceof Image) {
+                this.__backgroundImage = image
+            } else {
+                if(typeof image === "string") {
+                    let i = new Image();
+                    i.loaded = false;
+                    i.onload = function() {
+                        this.loaded = true;
+                        __that.__backgroundImage = this;
+                    }
+                    i.src = image;
+                    this.__backgroundImage = i;
+                }
+            }
+        },
+
         getDimensions: function () {
             return this.__dimensions;
         },
         setDimensions: function(dimensions) {
+            let spriteCount = this.getSpriteSheets().length;
             if(dimensions instanceof Array) {
-                this.__dimensions = dimensions;
+                if(dimensions.length == spriteCount || dimensions.length == 1) {
+                    this.__iterate = dimensions.length == spriteCount;
+                    for(let i = 0; i < spriteCount; i++) {
+                        this.__dimensions.push(dimensions[this.__iterate ? i : 0]);
+                    }
+                }  else {
+                    console.error('When setting up multiple spritesheets with different dimensions, the amount of spritesheets and dimensions must match.');
+                    this.__dimensions = [];
+                    return;
+                }
             } else if (dimensions instanceof Object) {
-                this.__dimensions.push(dimensions);
+                if(spriteCount == 1) {
+                    for(let i = 0; i < spriteCount; i++) {
+                        this.__dimensions.push(dimensions[0]);
+                    }
+                }
             }
         },
 
@@ -165,32 +257,37 @@ window.SpriteText = function (canvas,initProps) {
             }
         },
         // TODO: create properties, methods
-        initProperties: function(properties) {
+        initSettings: function(settings) {
             this.setDefaults();
-            if(properties != null) {
-                for(let p in properties) {
-                    this.setProperty(p,properties[p]);
+            if(settings != null) {
+                for(let p in settings) {
+                    this.setSetting(p,settings[p]);
                 }
             }
-            this.setProperty('maxHeight',this.getMaxHeight());
-            this.setProperty('fontCount', this.getSpriteSheets().length);
+            this.setSetting('maxHeight',this.getMaxHeight());
+            this.setSetting('fontCount', this.getSpriteSheets().length);
         },
         setDefaults: function() {
-            this.__properties = this.__defaults__;
+            this.__settings = this.__defaults__;
         },
 
-        getProperty: function(name) {
-            return this.__properties['name'];
+        getSetting: function(name) {
+            return this.__settings[name];
         },
-        setProperty: function(name, value) {
-            this.__properties['name'] = value;
+        setSetting: function(name, value) {
+            this.__settings[name] = value;
+        },
+
+        getRatio: function(){
+            return this.getSetting('fontSize') / this.getMaxHeight();
         },
 
         getMaxHeight: function() {
             let maxHeight = 0;
-            for(d in this.getDimensions()) {
-                for(i in d) {
-                    let h = d[i][1] - d[i][0];
+            let dims = !this.__iterate ? [this.getDimensions()[0]] : this.getDimensions();
+            for(d in dims) {
+                for(i in dims[d]) {
+                    let h = dims[d][i][3] - dims[d][i][2]; // Delta y
                     if(maxHeight < h) {
                         maxHeight = h;
                     }
@@ -199,39 +296,210 @@ window.SpriteText = function (canvas,initProps) {
             return maxHeight;
         },
 
+        getMaxWidth: function() {
+            let maxWidth = 0;
+            let dims = !this.__iterate ? [this.getDimensions()[0]] : this.getDimensions();
+            for(d in dims) {
+                for(i in dims[d]) {
+                    let w = dims[d][i][1] - dims[d][i][0]; // Delta x
+                    if(maxWidth < w) {
+                        maxWidth = w;
+                    }
+                }
+            }
+            return maxWidth;
+        },
+
         getLetter: function(l) {
-            let rand = Math.floor(Math.random() * this.getProperty('fontCount'));
+            let rand = Math.floor(Math.random() * this.getSetting('fontCount'));
             let letter = {};
             let dims = this.getDimensions()[rand][l];
 
             letter.image = this.getSpriteSheets()[rand];
             letter.sx = dims[0];
             letter.sy = dims[2];
-            letter.sWidth = dims[1] - dims[0];
-            letter.sHeight = dims[3] - dims[2];
+            letter.sWidth = dims[1];
+            letter.sHeight = dims[3];
 
             return letter;
+        },
+
+        getSpace: function() {
+            return this.getTotalHeight() * this.getSetting('whitespaceWidth');
+        },
+
+        clearElements: function() {
+            this.__elements__ = [];
         },
 
         addElement: function(el) {
             this.__elements__.push(el);
         },
 
+        getElements: function(){
+            return this.__elements__;
+        },
+
+        checkLoadedImages: function() {
+            let s = __that.getSpriteSheets();
+            let b = __that.getBackgroundImage();
+            b = b == null || b.loaded;
+            let allLoaded = true;
+            for(let i in s) {
+                if(!s[i].loaded || !b) {
+                    allLoaded = false;
+                }
+            }
+            if(!allLoaded) {
+                setTimeout(__that.checkLoadedImages, 1000);
+            } else{
+                __that.createTextElements();
+                __that.drawElements();
+                return;
+            }
+        },
+
+        reload: function(text) {
+            let canvas = this.getCanvas();
+            canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
+            this.setText(text);
+            this.createTextElements();
+            this.drawElements();
+        },
+
         createTextElements: function() {
+            this.clearElements();
             let text = this.getText();
             let textArray = text.split('');
             for(let i in textArray) {
                 if(textArray[i] == ' ') {
                     //TODO handle spaces
+                    this.addElement(
+                        new SpriteTextSpace());
                 }
                 else {
                     let letter = this.getLetter(textArray[i]);
                     this.addElement(
-                        new SpriteTextElement(letter));
+                        new SpriteTextCharacter(letter, this.getRatio()));
                 }
             }
-            console.log(this.__elements__);
+            
         },
+
+        drawElements: function() {
+            let bg = this.getBackgroundImage(),
+                canvas = this.getCanvas(),
+                bgRatio = canvas.width / bg.width;
+            
+            canvas.height = bg.height * bgRatio;
+
+            let cip = this.getCanvasInitialPosition(), //canvas initial position
+                hAlign = this.getSetting('horizontalAlignment'),
+                vAlign = this.getSetting('verticalAlignment'),
+                totalWidth = this.getTotalWidth(),
+                totalHeight = this.getTotalHeight(),
+                lm = (hAlign == 'center' ? (totalWidth / 2) : 
+                                            (hAlign == 'right' ? totalWidth : 0)),
+                top = cip.y - totalHeight / 2,
+                initPos = cip.x - lm
+                currPos = initPos,
+                hasRotation = this.getSetting('rotation') != 0,
+                el = this.getElements(),
+                ctx = canvas.getContext('2d');
+            
+                ctx.drawImage(bg,0,0,canvas.width, canvas.height);
+            
+            for(let i in el) {
+                switch(el[i].getType()) {
+                    case 'char': {
+                        let p = el[i].getProperties(),
+                            eHeight = totalHeight - p.dHeight,
+                            marginTop = vAlign == 'middle' ? eHeight / 2 : 
+                                vAlign == 'bottom' ? eHeight : 0
+                            dx = currPos,
+                            dy = top + marginTop;
+                        if(hasRotation){
+                            let rotationAngle = this.getRotationAngle();
+                            ctx.translate(dx + p.dWidth/2,dy + p.dHeight/2);
+                            ctx.rotate(rotationAngle);
+                            ctx.drawImage(p.image, p.sx, p.sy, p.sWidth, p.sHeight,
+                                -p.dWidth/2, -p.dHeight/2, p.dWidth, p.dHeight);
+                            ctx.rotate(-rotationAngle);
+                            ctx.translate(-1 * (dx + p.dWidth/2),-1 * (dy + p.dHeight/2));
+                        } else {
+                            ctx.drawImage(p.image, p.sx, p.sy, p.sWidth, p.sHeight,
+                                dx, dy, p.dWidth, p.dHeight);
+                        }
+                            currPos += p.dWidth;
+                        break;
+                    }
+                    case 'space' : {
+                        currPos += this.getSpace();
+                        break;
+                    }
+                }
+            }
+        },
+
+        getRotationAngle: function() {
+            let negative = this.getSetting('rotateBoth');
+            let angle = Math.random() * this.getSetting('rotation');
+            angle *= negative ? (Math.random() < 0.5 ? -1 : 1 ) : 1;
+            return angle * Math.PI / 180;
+        },
+
+        getTotalWidth: function() {
+            let w = 0,
+                elements = this.getElements(),
+                whiteSpace = this.getSpace();
+            for(let i in elements) {
+                switch(elements[i].getType()) {
+                    case 'char':
+                        w += elements[i].getWidth();
+                        break;
+                    case 'space':
+                        w += whiteSpace;
+                        break;
+                }
+            }
+            return w;
+        },
+
+        getFullWidth: function() {
+            let w = 0,
+                elements = this.getElements(),
+                whiteSpace = this.getMaxHeight() * this.getSetting('whitespaceWidth');
+            for(let i in elements) {
+                switch(elements[i].getType()) {
+                    case 'char':
+                        w += elements[i].getSourceWidth();
+                        break;
+                    case 'space':
+                        w += whiteSpace;
+                        break;
+                }
+            }
+            return w;
+        },
+
+        getTotalHeight: function() {
+            return this.getMaxHeight() * this.getRatio();
+        },
+
+        getCanvasWidth: function() {
+            return this.__canvas.width;
+        },
+        getCanvasHeight: function() {
+            return this.__canvas.height;
+        },
+
+        getCanvasInitialPosition: function() {
+            let initialPosition = this.getSetting('initialPosition');
+            return {
+                x: this.getCanvasWidth() * initialPosition[0],
+                y: this.getCanvasHeight() * initialPosition[1]
+            }
+        }
         // TODO: paint elements onto canvas
     };
 
